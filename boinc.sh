018bbe241b801ff165ac5e5985499fc12b5cd10a
KittenOnFire <bigshoush@gmail.com>
2016-09-03 23:13:38 +0200

    Update boinc.sh

--- boinc.sh
+++ boinc.sh
@@ -9,7 +9,7 @@ echo "Joindre un compte pour BOINC"
 echo "Quel est votre mot de passe pour le client BOINC sur cet ordinateur ?"
 read pass_client
 sudo echo $pass_client > /var/lib/boinc-client/gui_rpc_auth.cfg && echo "==> Password GUI inscrit dans le fichier gui_prc_auth.cfg" # Inscrire le password GUI dans le fichier idoine
-echo "Quelle est l'adresse du manager ? (exemple : https://am.statseb.fr/ ou https://bam.boincstats.com/"
+echo "Quelle est l'adresse du manager ? (exemple : http://am.statseb.fr/ ou http://bam.boincstats.com/"
 read manager
 echo "Quelle est l'adresse mail utilisée avec ce manager ?"
 read mail
@@ -17,12 +17,14 @@ echo "Quel est votre mot de passe sur ce manager ?"
 read pass_manager
 sudo boinccmd --host localhost --passwd $pass_client --join_acct_mgr $manager $mail $pass_manager && echo "==> Vous avez maintenant rejoint le manager $manager"
 
+sudo /etc/init.d/boinc-client restart
+
 sudo boinccmd --set_run_mode always && echo "==> BOINC tourne maintenant en permanence" # Utiliser BOINC en permanence
 
 sudo boinccmd --set_network_mode always && echo "==> BOINC à maintenant toujours accès à Internet" # Toujours être relié au réseau
 
 echo "Adresse ip de l'ordinateur hôte (ayant accès à celui-ci). Exemple : 192.168.1.10"
 read ip_master
-sudo echo $ip_master > /etc/boinc-client/remote_hosts.cfg && echo "IP de l'ordinateur hôte inscrite dans le fichier remote_hosts.cfg" # Inscrire l'IP de l'ordi hôte
+sudo echo $ip_master > /etc/boinc-client/remote_hosts.cfg && echo "==> IP de l'ordinateur hôte inscrite dans le fichier remote_hosts.cfg" # Inscrire l'IP de l'ordi hôte
 
 sudo /etc/init.d/boinc-client restart && echo "Client BOINC redémarré. Bon crunch !"
